46249b0b2e3ffcea382248717bad30fe035e06a9
GTOsss <sedmoy.mm@mail.ru>
2017-02-22 12:07:28 +0300

    v008

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,15 @@ document.addEventListener('DOMContentLoaded', Ready);
 function Ready(){
 	VK.init(function() { 
   		console.log('vk API initialization succeeded.');
-		groups = VK.groups.get("140286227", true, 0, 0, 0, 100, 5.62);
+		groups = VK.('groups.get', {
+			'user_id': 140286227,
+			'extended': true,
+			'filter': 0,
+			'fields': 0,
+			'offset': 0,
+			'count': 100,
+			'version': 5.62
+		});
 		console.log('callMethod groups.get');
 		console.log(groups);
 	}, function() { 
