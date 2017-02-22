885ac3049ea4a505bb18aa9eda5c4e3aba119b86
GTOsss <sedmoy.mm@mail.ru>
2017-02-22 10:55:36 +0300

    v003

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,11 @@ document.addEventListener('DOMContentLoaded', Ready);
 function Ready(){
 	VK.init(function() { 
   		console.log('vk API initialization succeeded.');
+		groups = VK.callMethod("groups.get", 140286227, true, 0, 0, 0, 100, 5.62);
+		console.log(groups);
 	}, function() { 
 		console.error('Error vk API initialization.');
 	}, '5.62'); 
 	
-	groups = VK.callMethod("groups.get", 140286227, true, 0, 0, 0, 100, 5.62);
-	console.log(groups);
+
 }
