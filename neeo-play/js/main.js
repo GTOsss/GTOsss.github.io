8796a9b96f9332c5e7ab73595612a3a75d65a4d9
Timofey Goncharov <gtosss@mail.ru>
2019-06-30 20:43:19 +0300

    Update.

--- neeo-play/js/main.js
+++ neeo-play/js/main.js
@@ -1,162 +1,296 @@
-function ready() {
-  var url1 = '/play1';
-  var url2 = '/play2';
+(function () {
+  function readyDOM() {
+    var neeoCome;
+    var videoBackground;
 
-  var foregroundNode = document.getElementById('foreground');
-  var link1 = document.getElementById('link1');
-  var link2 = document.getElementById('link2');
+    var url1 = '/play1';
+    var url2 = '/play2';
 
-  var neeoCame = false;
-  var neeoStartDisappear = false;
+    var loadingProgressTextNode = document.getElementById('loadingProgressText');
 
-  var neeoComeNode = document.getElementById('neeoCome');
-  var neeoStillNode = document.getElementById('neeoStill');
-  var neeoDisappear1Node = document.getElementById('neeoDisappear1');
-  var neeoDisappear2Node = document.getElementById('neeoDisappear2');
-  var neeoDisappearStartNode = document.getElementById('neeoDisappearStart');
-  // var backgroundNode = document.getElementById('background');
+    var foregroundNode = document.getElementById('foreground');
+    var link1 = document.getElementById('link1');
+    var link2 = document.getElementById('link2');
 
-  link1.addEventListener('click', onClick1);
-  link2.addEventListener('click', onClick2);
+    var neeoCame = false;
+    var neeoStartDisappear = false;
 
-  // var background = bodymovin.loadAnimation({
-  //   wrapper: backgroundNode,
-  //   renderer: 'canvas',
-  //   loop: true,
-  //   autoplay: true,
-  //   path: 'assets/background/data.json',
-  // });
+    var neeoComeNode = document.getElementById('neeoCome');
+    var neeoStillNode = document.getElementById('neeoStill');
+    var neeoDisappear1Node = document.getElementById('neeoDisappear1');
+    var neeoDisappear2Node = document.getElementById('neeoDisappear2');
+    var neeoDisappearStartNode = document.getElementById('neeoDisappearStart');
+    // var backgroundNode = document.getElementById('background');
 
-  var foreground = bodymovin.loadAnimation({
-    wrapper: foregroundNode,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'assets/foreground/data.json',
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
+    var mainBlockNode = document.getElementById('mainBlock');
+    var loadingBlockNode = document.getElementById('loadingBlock');
+
+    function setLoading(value) {
+      if (value) {
+        // mainBlockNode.classList.add('hidden');
+      } else {
+        loadingBlockNode.classList.add('loading-block_hidden');
+        setTimeout(function () {
+          loadingBlockNode.remove();
+        }, 1000);
+
+        videoBackground.play();
+        setTimeout(function () {
+          neeoCome.play();
+        }, 2500);
+      }
     }
-  });
 
-  var neeoCome = bodymovin.loadAnimation({
-    wrapper: neeoComeNode,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'assets/neeo-come/data.json',
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    }
-  });
+    setLoading(true);
 
-  var neeoStill = bodymovin.loadAnimation({
-    wrapper: neeoStillNode,
-    renderer: 'svg',
-    loop: true,
-    autoplay: false,
-    path: 'assets/neeo-still/data.json',
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    }
-  });
+    // script
+    // video
+    // 6*3 animations
+    var maxLoadProgress = 26;
 
-  var neeoDisappearStart = bodymovin.loadAnimation({
-    wrapper: neeoDisappearStartNode,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'assets/neeo-disappear-start/data.json',
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    }
-  });
+    var loadVideoPercent = 0;
 
-  var neeoDisappear1 = bodymovin.loadAnimation({
-    wrapper: neeoDisappear1Node,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'assets/neeo-disappear1/data.json',
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    }
-  });
+    var loadProgress = {
+      max: maxLoadProgress,
+      loaded: false,
+      _value: 0,
 
-  var neeoDisappear2 = bodymovin.loadAnimation({
-    wrapper: neeoDisappear2Node,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'assets/neeo-disappear2/data.json',
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    }
-  });
+      get percent() {
+        var currentPercent = this._value / this.max * 100;
+        currentPercent = Math.ceil(currentPercent);
+        return currentPercent > 100 ? 100 : currentPercent;
+      },
 
-  neeoCome.addEventListener('complete', completeCome);
+      set value(value) {
+        this._value = value;
 
-  function onClick1(e) {
-    e.preventDefault();
+        loadingProgressTextNode.innerHTML = this.percent + '%';
 
-    if (neeoCame && !neeoStartDisappear) {
-      neeoStartDisappear = true;
-      neeoStillNode.classList.toggle('hidden');
-      neeoDisappearStartNode.classList.toggle('hidden');
+        if ((this.percent === 100) && (this.loaded === false)) {
+          this.loaded = true;
+          setLoading(false);
+        }
+      },
 
-      neeoDisappearStart.addEventListener('complete', function() {
-        neeoDisappearStartNode.classList.toggle('hidden');
-        neeoDisappear1Node.classList.toggle('hidden');
-        neeoDisappearStart.destroy();
-        neeoDisappear1.addEventListener('complete', function () {
-          redirect(url1);
-        });
-        neeoDisappear1.play();
+      get value() {
+        return this._value;
+      },
+    };
+
+    function readyLottieJS() {
+      link1.addEventListener('click', onClick1);
+      link2.addEventListener('click', onClick2);
+
+      // var background = bodymovin.loadAnimation({
+      //   wrapper: backgroundNode,
+      //   renderer: 'canvas',
+      //   loop: true,
+      //   autoplay: true,
+      //   path: 'assets/background/data.json',
+      // });
+
+      function readyAnimation() {
+        loadProgress.value += 1;
+      }
+
+      var foreground = bodymovin.loadAnimation({
+        wrapper: foregroundNode,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/foreground/data.json',
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        },
       });
 
-      neeoDisappearStart.play();
-    }
-  }
+      foreground.addEventListener('config_ready', readyAnimation);
+      foreground.addEventListener('data_ready', readyAnimation);
+      foreground.addEventListener('DOMLoaded', readyAnimation);
+      foreground.addEventListener('segmentStart', readyAnimation);
+      foreground.addEventListener('loaded_images', readyAnimation);
 
-  function onClick2(e) {
-    e.preventDefault();
-    if (neeoCame && !neeoStartDisappear) {
-      neeoStartDisappear = true;
-      neeoStillNode.classList.toggle('hidden');
-      neeoDisappearStartNode.classList.toggle('hidden');
-
-      neeoDisappearStart.addEventListener('complete', function() {
-        neeoDisappearStartNode.classList.toggle('hidden');
-        neeoDisappear2Node.classList.toggle('hidden');
-        neeoDisappearStart.destroy();
-        // neeoDisappear2.addEventListener('complete', function () {
-        //   redirect(url2);
-        // });
-        setTimeout(function() {
-          redirect(url2);
-        }, 4200);
-        neeoDisappear2.play();
+      neeoCome = bodymovin.loadAnimation({
+        wrapper: neeoComeNode,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'assets/neeo-come/data.json',
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        }
       });
 
-      neeoDisappearStart.play();
+      neeoCome.addEventListener('config_ready', readyAnimation);
+      neeoCome.addEventListener('data_ready', readyAnimation);
+      neeoCome.addEventListener('DOMLoaded', readyAnimation);
+      neeoCome.addEventListener('loaded_images', readyAnimation);
+
+      var neeoStill = bodymovin.loadAnimation({
+        wrapper: neeoStillNode,
+        renderer: 'svg',
+        loop: true,
+        autoplay: false,
+        path: 'assets/neeo-still/data.json',
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        }
+      });
+
+      neeoStill.addEventListener('config_ready', readyAnimation);
+      neeoStill.addEventListener('data_ready', readyAnimation);
+      neeoStill.addEventListener('DOMLoaded', readyAnimation);
+      neeoStill.addEventListener('loaded_images', readyAnimation);
+
+      var neeoDisappearStart = bodymovin.loadAnimation({
+        wrapper: neeoDisappearStartNode,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'assets/neeo-disappear-start/data.json',
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        }
+      });
+
+      neeoDisappearStart.addEventListener('config_ready', readyAnimation);
+      neeoDisappearStart.addEventListener('data_ready', readyAnimation);
+      neeoDisappearStart.addEventListener('DOMLoaded', readyAnimation);
+      neeoDisappearStart.addEventListener('loaded_images', readyAnimation);
+
+      var neeoDisappear1 = bodymovin.loadAnimation({
+        wrapper: neeoDisappear1Node,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'assets/neeo-disappear1/data.json',
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        }
+      });
+
+      neeoDisappear1.addEventListener('config_ready', readyAnimation);
+      neeoDisappear1.addEventListener('data_ready', readyAnimation);
+      neeoDisappear1.addEventListener('DOMLoaded', readyAnimation);
+      neeoDisappear1.addEventListener('loaded_images', readyAnimation);
+
+      var neeoDisappear2 = bodymovin.loadAnimation({
+        wrapper: neeoDisappear2Node,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'assets/neeo-disappear2/data.json',
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        }
+      });
+
+      neeoDisappear2.addEventListener('config_ready', readyAnimation);
+      neeoDisappear2.addEventListener('data_ready', readyAnimation);
+      neeoDisappear2.addEventListener('DOMLoaded', readyAnimation);
+      neeoDisappear2.addEventListener('loaded_images', readyAnimation);
+
+      neeoCome.addEventListener('complete', completeCome);
+
+      function onClick1(e) {
+        e.preventDefault();
+
+        if (neeoCame && !neeoStartDisappear) {
+          neeoStartDisappear = true;
+          neeoStillNode.classList.toggle('hidden');
+          neeoDisappearStartNode.classList.toggle('hidden');
+
+          neeoDisappearStart.addEventListener('complete', function () {
+            neeoDisappearStartNode.classList.toggle('hidden');
+            neeoDisappear1Node.classList.toggle('hidden');
+            neeoDisappearStart.destroy();
+            neeoDisappear1.addEventListener('complete', function () {
+              redirect(url1);
+            });
+            neeoDisappear1.play();
+          });
+
+          neeoDisappearStart.play();
+        }
+      }
+
+      function onClick2(e) {
+        e.preventDefault();
+        if (neeoCame && !neeoStartDisappear) {
+          neeoStartDisappear = true;
+          neeoStillNode.classList.toggle('hidden');
+          neeoDisappearStartNode.classList.toggle('hidden');
+
+          neeoDisappearStart.addEventListener('complete', function () {
+            neeoDisappearStartNode.classList.toggle('hidden');
+            neeoDisappear2Node.classList.toggle('hidden');
+            neeoDisappearStart.destroy();
+            // neeoDisappear2.addEventListener('complete', function () {
+            //   redirect(url2);
+            // });
+            setTimeout(function () {
+              redirect(url2);
+            }, 4200);
+            neeoDisappear2.play();
+          });
+
+          neeoDisappearStart.play();
+        }
+      }
+
+      function completeCome() {
+        neeoComeNode.classList.toggle('hidden');
+        neeoStillNode.classList.toggle('hidden');
+        neeoCome.destroy();
+        neeoStill.play();
+        neeoCame = true;
+      }
+
+      function redirect(url) {
+        window.location = url;
+      }
     }
+
+    var scriptLottie = document.getElementById('scriptLottie');
+    videoBackground = document.getElementById('videoBackground');
+
+    function nextLoadState() {
+      if (loadProgress.value < 2) {
+        loadProgress.value += 1;
+      }
+
+      if (loadProgress.value === 2) {
+        readyLottieJS();
+      }
+    }
+
+    scriptLottie.addEventListener('load', function () {
+      nextLoadState();
+    });
+
+    function startObserveProgressVideo() {
+      var intervalId = setInterval(updateLoadVideoPercent, 200);
+
+      function updateLoadVideoPercent() {
+        if (videoBackground.buffered.length) {
+          var maxLengthVideo = videoBackground.duration;
+          var currentLengthVideo = videoBackground.buffered.end(0);
+          loadVideoPercent = Math.floor(currentLengthVideo / maxLengthVideo * 100);
+          loadVideoPercent = loadVideoPercent > 100 ? 100 : loadVideoPercent;
+        } else {
+          loadVideoPercent = 0;
+        }
+
+        if (loadVideoPercent === 100) {
+          clearInterval(intervalId);
+          nextLoadState();
+        }
+      }
+    }
+
+    startObserveProgressVideo();
   }
 
-  function completeCome() {
-    neeoComeNode.classList.toggle('hidden');
-    neeoStillNode.classList.toggle('hidden');
-    neeoCome.destroy();
-    neeoStill.play();
-    neeoCame = true;
-  }
+  document.addEventListener('DOMContentLoaded', readyDOM, false);
 
-  function redirect(url) {
-    console.log(url);
-    window.location = url;
-  }
-
-  setTimeout(() => {
-    neeoCome.play();
-  }, 0)
-}
-
-document.addEventListener('DOMContentLoaded', ready, false);
+})();
